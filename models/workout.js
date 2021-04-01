ba66c4febd6d67bcc66699b6d326374c80a8863c
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-01 19:59:51 -0400

    updated workout.js

---
+++ models/workout.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  // day exercises
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  type: {
+      type: String,
+      trim: true, 
+      required: "Enter a workout type"
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter the name for the workout"
+  },
+  duration: {
+    type: Number,
+  },
+  weight: {
+    type: Number,
+  },
+  reps: {
+    type: Number,
+  },
+  sets: {
+    type: Number,
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
