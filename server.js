495ec338a3beced6b2e18517607362e938978a13
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-07 18:23:31 -0400

    updated mongose connection

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  // useCreateIndex: true,
-  // useUnifiedTopology: true
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
 htmlRouter(app);
